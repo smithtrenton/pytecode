e5d3e0c16e9c41b264d8cb85addfd56ddefadc50
Trenton Smith <tlsdude@gmail.com>
2020-04-17 21:37:06 -0600

    base py files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+with open('README') as f:
+    long_desc = f.read()
+
+setup(
+    name='pytecode',
+    version='0.0.1',
+    url='github.com/smithtrenton/pytecode',
+    license='LICENSE',
+    description='Python library for parsing and analyzing JVM bytecode',
+    long_description=long_desc,
+    packages=['pytecode']
+)
