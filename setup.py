344d0a68c3a33aa1e0392c7cfedf1a31345e0c69
Trenton Smith <tlsdude@gmail.com>
2026-03-21 23:39:25 -0600

    chore(setup): require Python >=3.14

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-with open("README") as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_desc = f.read()
 
 setup(
@@ -11,4 +11,10 @@ setup(
     description="Python library for parsing and analyzing JVM bytecode",
     long_description=long_desc,
     packages=["pytecode"],
+    python_requires=">=3.14",
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.14",
+    ],
 )
